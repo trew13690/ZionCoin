26a6014023a9701f9ec6701797b0f5e4aa371d64
Alex <trew13690@live.com>
2019-06-11 15:52:12 -0700

    Other Changes

---
+++ ZionApp/blockchain.py
@@ -0,0 +1,101 @@
+
+import hashlib
+import json
+from time import time
+
+
+class BlockChain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+            Create a new Block in the Blockchain 
+
+            :param proof: <int> The proof given by the Proof of Work Algorithm
+            :param previous_hash: (Optional) <str> Hash of previous Block
+            :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain)+ 1,
+            'timestamp': time(),
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+
+        }
+        self.current_transaction = []
+        self.chain.append(block)
+        return block
+
+        
+    
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Create a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transaction.append({
+            'sender': sender,
+            'recipeient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index']+ 1
+
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict> Block
+        :return <str>
+        """
+
+        block_string= json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algoritm:
+        
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof,proof):
+        """
+        Validates the Proof: Does Hash() contain 4 leading zeros?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if corretn, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
